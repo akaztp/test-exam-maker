926f8e195821b3ed447c131809d862c6036467e2
José Proença <joseantonioproenca@gmail.com>
2017-09-07 14:43:42 +0100

    Added a jasmine matcher for deep equality.

---
+++ src/app/utils/deep-equal.ts
@@ -0,0 +1,24 @@
+﻿export function deepEqual(a: any, b: any): boolean
+{
+	return levelEqual(a, b, deepEqual);
+}
+
+function levelEqual(a: any, b: any, comparator: (a, b) => boolean): boolean
+{
+	const typea = typeof a;
+	if (typea !== typeof b)
+		return false;
+	if (typea !== 'object')
+		return a == b;
+
+	if (a === b)
+		return true;
+
+	if (!a || !b)
+		return false;
+
+	const keysa = Object.keys(a);
+	if (keysa.length !== Object.keys(b).length)
+		return false;
+	return keysa.findIndex(k => !comparator(a[k], b[k])) === -1;
+}
---
+++ src/app/utils/jasmine-matchers.ts
@@ -0,0 +1,13 @@
+﻿import { deepEqual } from './deep-equal';
+
+export const deepEqualMatcher: jasmine.CustomMatcherFactories = {
+	toDeepEqual: function (util, customEqualityTesters)
+	{
+		return {
+			compare: function (actual, expected)
+			{
+				return { pass: deepEqual(actual, expected) };
+			}
+		}
+	}
+}
